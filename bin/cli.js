c72ccde8d3e9f2516010b6876237d1789c45525b
Asaaf Hadad <asaf147369@gmail.com>
2018-07-08 23:08:49 +0300

    screenshot taken

--- bin/cli.js
+++ bin/cli.js
@@ -1,7 +1,18 @@
 #!/usr/bin/env node
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({});
+  const page = await browser.newPage();
+  await page.goto('http://www.google.com');
+  await page.screenshot({path: 'scrreenshot.jpg'});
+
+  await browser.close();
+})();
 
 const figlet = require('figlet');
 
 let fig = figlet.textSync('Capture');
 
 console.log(fig);
+
