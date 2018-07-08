79846e615916f99b727db0b58be07d507f3b1645
unknown <mark22312@gmail.com>
2018-07-08 19:07:41 +0300

    Removed Typescript

---
+++ bin/cli.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+const figlet = require('figlet');
+
+let fig = figlet.textSync('Capture');
+
+console.log(fig);
---
+++ src/index.js
@@ -0,0 +1,4 @@
+
+const yargs = require('yargs').argv;
+
+console.log(yargs );
