bd550a61bd9d69f699ee26c536c82bbb1dcd1777
unknown <mark22312@gmail.com>
2018-07-09 22:21:44 +0300

    Added index.js

--- bin/cli.js
+++ bin/cli.js
@@ -1,17 +1,18 @@
 #!/usr/bin/env node
-
+const main = require('../lib/index.js');
 const figlet = require('figlet');
 let fig = figlet.textSync('Capture.js');
 console.log(fig);
-const yargs = argv = require('yargs')
-	.usage('Usage: capture -u <URL> -r [comma-seperated resolutions] -p')
+const yargs = require('yargs')
+	.usage('Usage: capture -u <URL> -r [comma-seperated resolutions] -p \"test/capture\"')
 	.demand(['u'])
 	.alias('u', 'url')
 	.alias('r', 'resolutions')
 	.alias('p', 'path')
 	.describe('u', 'URL to test.')
-	.describe('r', 'Comma-seperated resolution list (for example: 1920x1200,1024x768)')
+	.describe('r', 'Comma-seperated resolutions')
 	.describe('p', 'fullpath to save the file to')
 	.argv;
-    
+	
+main(yargs.u);
 
