2d055597d1018a7a1e4e14c070bf66f6656cc17b
Marik Sh | Ballaba-it <mark@ballaba-it.com>
2018-07-09 19:50:45 +0300

    Refactoring Folders  - Renamed dist to lib  - Added yargs description

--- bin/cli.js
+++ bin/cli.js
@@ -1,7 +1,17 @@
 #!/usr/bin/env node
 
 const figlet = require('figlet');
-
-let fig = figlet.textSync('Capture');
-
+let fig = figlet.textSync('Capture.js');
 console.log(fig);
+const yargs = argv = require('yargs')
+	.usage('Usage: capture -u <URL> -r [comma-seperated resolutions] -p')
+	.demand(['u'])
+	.alias('u', 'url')
+	.alias('r', 'resolutions')
+	.alias('p', 'path')
+	.describe('u', 'URL to test.')
+	.describe('r', 'Comma-seperated resolution list (for example: 1920x1200,1024x768)')
+	.describe('p', 'fullpath to save the file to')
+	.argv;
+    
+
