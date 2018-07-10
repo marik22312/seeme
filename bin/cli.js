d7565d408f86540da2fd5e9b3921771c3bbd6605
Marik Sh | Ballaba-it <mark@ballaba-it.com>
2018-07-10 19:17:21 +0300

    Added Config  - Added Config For Defaults  - Better yargs definition

--- bin/cli.js
+++ bin/cli.js
@@ -1,17 +1,28 @@
 #!/usr/bin/env node
-
+const config = require('../lib/config.js');
 const figlet = require('figlet');
 let fig = figlet.textSync('Capture.js');
 console.log(fig);
 const yargs = argv = require('yargs')
 	.usage('Usage: capture -u <URL> -r [comma-seperated resolutions] -p')
-	.demand(['u'])
-	.alias('u', 'url')
-	.alias('r', 'resolutions')
-	.alias('p', 'path')
-	.describe('u', 'URL to test.')
-	.describe('r', 'Comma-seperated resolution list (for example: 1920x1200,1024x768)')
-	.describe('p', 'fullpath to save the file to')
+	.option('url', {
+		alias: 'u',
+		demand: true,
+		type: 'string',
+		describe: 'URL to test'
+	})
+	.option('res', {
+		alias: 'r',
+		default: config.defaults.PATH,
+		demand: true,
+		type: 'string',
+		describe: 'fullpath to save the file to'
+	})
+	.option('path', {
+		alias: 'p',
+		demand: true,
+		default: config.defaults.RESOLUTIONS,
+		type: 'string',
+		describe: 'omma-seperated resolution list'
+	})
 	.argv;
-    
-
