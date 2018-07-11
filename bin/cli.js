7461a895a2f327e1c900828c1e7c0e24e3a74edb
Marik Sh | Ballaba-it <mark@ballaba-it.com>
2018-07-11 19:53:36 +0300

    Added Options  - Added landscape option  - Added grid option

--- bin/cli.js
+++ bin/cli.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 const config = require('../lib/config.js');
 const figlet = require('figlet');
+
 let fig = figlet.textSync('Capture.js');
 console.log(fig);
+
 const yargs = argv = require('yargs')
-	.usage('Usage: capture -u <URL> -r [comma-seperated resolutions] -p')
+	.usage('Usage: capture -u <URL> -r [comma-seperated resolutions] -p "./screenshots"')
 	.option('url', {
 		alias: 'u',
 		demand: true,
@@ -14,15 +16,25 @@ const yargs = argv = require('yargs')
 	.option('res', {
 		alias: 'r',
 		default: config.defaults.PATH,
-		demand: true,
 		type: 'string',
 		describe: 'fullpath to save the file to'
 	})
 	.option('path', {
 		alias: 'p',
-		demand: true,
 		default: config.defaults.RESOLUTIONS,
 		type: 'string',
 		describe: 'omma-seperated resolution list'
+	})
+	.option('landscape', {
+		alias: 'l',
+		default: false,
+		type: 'boolean',
+		describe: 'Mobile also captures landscape'
+	})
+	.option('grid', {
+		alias: 'g',
+		default: false,
+		type: 'boolean',
+		describe: 'Show grid on photo [Unreleased]'
 	})
 	.argv;
